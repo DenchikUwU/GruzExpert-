01c88b4bc662806bf644f0f53e26bb2296d032cb
Denchik <129522239+DenchikUwU@users.noreply.github.com>
2026-09-10 16:37:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /* Replace these public temporary details before publishing. SMTP credentials must never be placed here. */
 window.GRUZEXPERT_CONFIG = {
-  phoneDisplay: '+370 631 54 171',
-  phoneHref: 'tel:+37063154171',
+  phoneDisplay: '+370 662 42 235',
+  phoneHref: 'tel:+37066242235',
   email: 'sidach3@gmail.com',
   heroImage: "url('assets/gruzexpert-van.png')",
   mapImage: "url('assets/gruzexpert-van.png')"
